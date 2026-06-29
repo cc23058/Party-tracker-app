64a01112a71c0f8444e9e1a773317f444edeab57
cc23058 <cc23058.mbc@email.school.nz>
2026-06-29 13:01:13 +1200

    Pricing

--- Partyapp.py
+++ Partyapp.py
@@ -12,8 +12,9 @@ frame.place(rely = 0.5, relx = 0.5, anchor = "center")
 
 #Creating menu of party items
 #Selection of Item & pricing
-tk.Label(frame, text="Item").grid(row=1, column=0, padx=10, pady=5, sticky="e")
-items = ["Balloons", 25, "Party Hats", 12, "Confetti", 15,"Music", 5, "Food", 20,"Drinks", 15]
+tk.Label(frame, text="Items:").grid(row=1, column=0, padx=10, pady=5, sticky="e")
+items = ["Balloons", "Party Hats", "Confetti", "Music", "Food", "Drinks"]
+items_price = [1.00, 2.50, 0.50, 100.00, 200.00, 150.00]
 selected_item = tk.StringVar(value=items[0])
 item_menu = tk.OptionMenu(frame, selected_item, *items)
 item_menu.grid(row=3, column=1, padx=10, pady=5)
