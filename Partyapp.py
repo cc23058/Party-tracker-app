474b6a32e92d123cc588db969152131bc2d0f872
cc23058 <cc23058.mbc@email.school.nz>
2026-06-30 12:03:16 +1200

    quantity

--- Partyapp.py
+++ Partyapp.py
@@ -9,6 +9,10 @@ root.geometry("1000x500")
 frame = tk.Frame(root)
 frame.pack()
 frame.place(rely = 0.5, relx = 0.5, anchor = "center")
+#The Quantity of items
+quantity_max = 500
+quantity_min = 1
+tk.Entry(frame, text="quantity").grid(row=2, column=3, padx=10, pady=5, sticky="e")
 
 #Selection of Item & pricing
 tk.Label(frame, text="Items:").grid(row=1, column=0, padx=10, pady=5, sticky="e")
